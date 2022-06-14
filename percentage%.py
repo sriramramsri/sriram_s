ade1937f221954459abe835d77c59c0a4772e77f
sriramramsri <106534448+sriramramsri@users.noreply.github.com>
2022-06-14 09:55:49 +0530

    Add files via upload

---
+++ percentage%.py
@@ -0,0 +1,8 @@
+#calculating the pecentage using python using variable "print"
+
+a=int(input("Enter the percentage:"))
+b=int(input("Enter the amount:"))
+percentage=(b*a)/100
+print(percentage)
+        
+
